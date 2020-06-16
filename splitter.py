9cc50fd2f4a3e602a763fb98abefc699e9c62c74
Sami Amer <59567234+samiatMIT@users.noreply.github.com>
2020-06-16 15:31:07 -0400

    split_vids function now works properly for a list of paths

--- splitter.py
+++ splitter.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import time
 import copy
+import statistics
 
 # os.chdir('F:\\Work\\VidSplit')
 # print(os.getcwd())
@@ -27,6 +28,21 @@ def merge_dics(dic1, dic2):
     return output
 
 
+def get_p_s(path, nameOnly = False): ## NOTE: This may need to be edited
+    pathR = path[::-1]
+    for index,char in enumerate(pathR):
+        if char == '\\':
+            num = len(path) - index
+            videopath = path[num:]
+            videopath = videopath[:7]
+            if nameOnly:
+                break
+            videopath = "F:\\Work\\VidSplit\\Video\\" + videopath[:3] + '\\' + videopath[4:8] + "\\" + videopath +".mp4" # just change this line to make it point to video path
+            break
+    
+    return videopath   
+
+
 def import_data(file_name):
 
     distarcted, idle, Satisfied, on_task, off_tsak, Bored, Confused, focused = (
@@ -40,6 +56,7 @@ def import_data(file_name):
         [],
     )
     with open(file_name, "r") as f:
+        videopath = get_p_s(file_name)
         for line in f:
             line = line.split("\t")
             del line[1]
@@ -48,21 +65,21 @@ def import_data(file_name):
                 continue
             else:
                 if line[4] == "distarcted":
-                    distarcted.append((file_name, line[1], line[3])) ## TODO: fix filename, its currently adding txt file name instead of video name
+                    distarcted.append((videopath, line[1], line[3]))
                 elif line[4] == "idle":
-                    idle.append((file_name, line[1], line[3]))
+                    idle.append((videopath, line[1], line[3]))
                 elif line[4] == "Satisfied":
-                    Satisfied.append((file_name, line[1], line[3]))
+                    Satisfied.append((videopath, line[1], line[3]))
                 elif line[4] == "Bored":
-                    Bored.append((file_name, line[1], line[3]))
+                    Bored.append((videopath, line[1], line[3]))
                 elif line[4] == "Confused":
-                    Confused.append((file_name, line[1], line[3]))
+                    Confused.append((videopath, line[1], line[3]))
                 elif line[4] == "focused":
-                    focused.append((file_name, line[1], line[3]))
+                    focused.append((videopath, line[1], line[3]))
                 elif line[4] == "on-task":
-                    on_task.append((file_name, line[1], line[3]))
+                    on_task.append((videopath, line[1], line[3]))
                 elif line[4] == "off-tsak":
-                    off_tsak.append((file_name, line[1], line[3]))
+                    off_tsak.append((videopath, line[1], line[3]))
                 else:
                     print('unknown error with line: ' + str(line))
     
@@ -103,7 +120,7 @@ def split_and_save(rootdir, name, start, duration, orig):
             "-t",
             duration,
             "-c:v",
-            "libx264",
+            "h264_nvenc",
             "-qp",
             "16",
             name,
@@ -119,38 +136,93 @@ def split_and_save(rootdir, name, start, duration, orig):
 def split_vids(Attention, Behavior, Emotion, rootdir):
     counter = 0
     for tup in Attention["distracted"]:
-        counter += 1
+        counter+=1
+        videoName = get_p_s(tup[0], True) + '_' + str(counter)
         name = (
             "F:\\Work\\VidSplit\\ExampleOut\\Attention\\distracted\\a_D_"
-            + str(counter)
+            + videoName
             + ".mp4"
-        )  ## TODO: find a way to change name based on original file
-
+        )  
         split_and_save(
             rootdir, name, tup[1], tup[2], tup[0]
         )  ## tup[0] so that multiple files can be used at once
     for tup in Attention["idle"]:
-        counter += 1
+        counter+=1
+        videoName = get_p_s(tup[0], True) + '_' + str(counter)
         name = (
             "F:\\Work\\VidSplit\\ExampleOut\\Attention\\idle\\a_I_"
-            + str(counter)
+            + videoName
             + ".mp4"
-        )  ## TODO: find a way to change name based on original file
-
+        ) 
         split_and_save(
             rootdir, name, tup[1], tup[2], tup[0]
         )  ## tup[0] so that multiple files can be used at once
-    for tup in Behavior["on-task"]:
+
+    for tup in Attention["focused"]:
         counter += 1
+        videoName = get_p_s(tup[0], True) + '_' + str(counter)
+        name = (
+            "F:\\Work\\VidSplit\\ExampleOut\\Attention\\focused\\a_F"
+            + videoName
+            + ".mp4"
+        )
+        split_and_save(rootdir, name, tup[1], tup[2],tup[0])
+
+    for tup in Behavior["on-task"]:
+        counter+=1
+        videoName = get_p_s(tup[0], True) + '_' + str(counter)
         name = (
             "F:\\Work\\VidSplit\\ExampleOut\\Behavior\\on-task\\b_N_"
-            + str(counter)
+            + videoName
             + ".mp4"
-        )  ## TODO: find a way to change name based on original file
-
+        )  
         split_and_save(
             rootdir, name, tup[1], tup[2], tup[0]
         )  ## tup[0] so that multiple files can be used at once
+
+    for tup in Behavior['off-task']:
+        counter+=1
+        videoName = get_p_s(tup[0], True) + '_' + str(counter)
+        name =(
+            "F:\\Work\\VidSplit\\ExampleOut\\Behavior\\off-task\\b_F"
+            + videoName
+            +".mp4"
+        )
+        split_and_save(
+            rootdir, name, tup[1], tup[2], tup[0]
+        )
+    
+    for tup in Emotion['satisfied']:
+        counter += 1
+        videoName = get_p_s(tup[0], True) + '_' + str(counter)
+        name =(
+            "F:\\Work\\VidSplit\\ExampleOut\\Emotion\\satisfied\\e_S"
+            + videoName
+            + ".mp4"
+        )
+        split_and_save(
+            rootdir,name,tup[1],tup[2],tup[0]
+        )
+    for tup in Emotion['confused']:
+        counter += 1
+        videoName = get_p_s(tup[0], True) + '_' + str(counter)
+        name =(
+            "F:\\Work\\VidSplit\\ExampleOut\\Emotion\\confused\\e_C"
+            + videoName
+            + ".mp4"
+        )
+        split_and_save(
+            rootdir,name,tup[1],tup[2],tup[0])
+    for tup in Emotion['bored']:
+        counter += 1
+        videoName = get_p_s(tup[0], True) + '_' + str(counter)
+        name =(
+            "F:\\Work\\VidSplit\\ExampleOut\\Emotion\\bored\\e_B"
+            + videoName
+            + ".mp4"
+        )
+        split_and_save(
+            rootdir,name,tup[1],tup[2],tup[0])
 
 def get_durations(a, b, e):
     ## NOTE: Duration is the third element of the tuple
@@ -169,21 +241,21 @@ def get_durations(a, b, e):
     Emotion = {"bored": Bored, "confused": Confused, "satisfied": Satisfied}
     ## TODO: cycle through tuple and pull durations into their respective dictionaries.  run functionless to get tallies
     for tup in a['distracted']:
-        Attention['distracted'].append(tup[2])
+        Attention['distracted'].append(float(tup[2]))
     for tup in a['idle']:
-        Attention['idle'].append(tup[2])
+        Attention['idle'].append(float(tup[2]))
     for tup in a['focused']:
-        Attention['focused'].append(tup[2])
+        Attention['focused'].append(float(tup[2]))
     for tup in b['off-task']:
-        Behavior['off-task'].append(tup[2])
+        Behavior['off-task'].append(float(tup[2]))
     for tup in b['on-task']:
-        Behavior['on-task'].append(tup[2])
+        Behavior['on-task'].append(float(tup[2]))
     for tup in e['bored']:
-        Emotion['bored'].append(tup[2])
+        Emotion['bored'].append(float(tup[2]))
     for tup in e['confused']:
-        Emotion['confused'].append(tup[2])
+        Emotion['confused'].append(float(tup[2]))
     for tup in e['satisfied']:
-        Emotion['satisfied'].append(tup[2])
+        Emotion['satisfied'].append(float(tup[2]))
 
     return Attention, Behavior, Emotion
 
@@ -197,6 +269,39 @@ if __name__ == "__main__":
     # print(d3)
     paths = import_paths_from_txt("paths.txt")
     a, b, e = import_data_multiple(paths)
-    # split_vids(a,b,e, 'F:\\Work\\VidSplit\\') 
-    ad,bd,ed = get_durations(a,b,e)
+    split_vids(a,b,e, 'F:\\Work\\VidSplit\\') 
+    # ad,bd,ed = get_durations(a,b,e)
+    # lessThan = 0
+    # moreThan = 0 ## if a clip == 1, it is counted as less than
+    # withoutList = []
+    # allList = []
+    # for duration in ad['distracted']:
+    #     if duration > 1.0:
+    #         moreThan += 1
+    #         withoutList.append(duration)
+    #         allList.append(duration)
+    #     elif duration <= 1.0:
+    #         lessThan += 1
+    #         allList.append(duration)
+    #     else:
+    #         print('unknown error with: ' + str(duration))
+    # withoutAvg = statistics.mean(withoutList)
+    # allAvg = statistics.mean(allList)
+    # withoutMed = statistics.median(withoutList)
+    # allMed = statistics.median(allList)
+    # maximum = max(allList)
+    # withoutMin = min(withoutList)
+    # allMin = min(allList)
+    
+    # print(
+    #     "# less than 1s: " + str(lessThan) + '\n' +
+    #     "# more than 1s: " + str(moreThan) + '\n' +
+    #     'Avg Duration w/o less than 1s: ' + str(withoutAvg) + '\n' +
+    #     "Avg Duration with less than 1s: " + str(allAvg) + '\n' +
+    #     "Median Duration w/o less than 1s: " + str(withoutMed) + '\n' +
+    #     "Median duration with less than 1s: " + str(allMed) + '\n' +
+    #     "Max Duration: " + str(maximum) + '\n' +
+    #     "Minimum w/o less than 1s: " + str(withoutMin) + '\n'+
+    #     "Minimum with less than 1s: " + str(allMin) + '\n' 
+    # )
     
